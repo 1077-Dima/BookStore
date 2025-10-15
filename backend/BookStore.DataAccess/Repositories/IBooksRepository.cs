6e2fdf31b0394eec4eb6cd1d472c159d50c27f90
Dima <d2118911@gmail.com>
2025-10-15 16:30:52 +0200

    Add .gitignore to ignore .vs folder

---
+++ backend/BookStore.DataAccess/Repositories/IBooksRepository.cs
@@ -0,0 +1,13 @@
+﻿using BookStore.Core.Models;
+
+namespace BookStore.DataAccess.Repositories
+{
+    public interface IBooksRepository
+    {
+        Task<Guid> Create(Book book);
+        Task<Guid> Delete(Guid id);
+        Task<List<Book>> Get();
+        Task<List<Book>> GetBooks();
+        Task<Guid> Update(Guid id, string title, string description, decimal price);
+    }
+}
