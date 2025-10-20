c2b25d852cede558493d6600eb1b437a4657652c
Dima <d2118911@gmail.com>
2025-10-20 09:33:38 +0200

    Fix: remove submodule bookstore and add as regular folder

---
+++ frontend/bookstore/app/components/Books.tsx
@@ -0,0 +1,33 @@
+import Button from "antd/es/button/button";
+import { CardTitle } from "./Cardtitle";
+import Card from "antd/es/card/Card";
+import { Flex } from "antd";
+
+interface Props {
+    books: Book[]
+    handleDelete: (id: string) => void;
+    handleOpen: (book: Book) => void;
+}
+
+export const Books = ({books, handleDelete, handleOpen}: Props) => {
+    return (
+        <div className="cards">
+            {books.map((book : Book) => (
+                <Card key={book.id}
+                 title={<CardTitle title={book.title} price={book.price} />}
+                  bordered={false}
+                   >
+                    <p>{book.description}</p>
+                    <div className="card__buttons">
+                        <Button onClick={() => handleOpen(book)} style={{ Flex: 1 }}>
+                            Edit
+                        </Button>
+                        <Button onClick={() => handleDelete(book.id)} style={{ Flex: 1 }}>
+                            Delete
+                        </Button>
+                    </div>
+                    </Card>
+            ))}
+        </div>
+    )
+}
---
+++ frontend/bookstore/app/page.tsx
@@ -0,0 +1,5 @@
+export default function Home() {
+  return (
+    <h1>Wellcome to book store</h1>
+  );
+}
